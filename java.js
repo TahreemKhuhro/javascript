fbaab386a9ca638ea7489d6da971cd235357f1f6
Tahreem <tahreemsaad15@gmail.com>
2024-11-28 03:34:49 -0800

    first commit

---
+++ java.js
@@ -0,0 +1,47 @@
+// if else
+let age = 16;
+if(age>18){
+    console.log("You can caste vote");
+}
+else{
+    console.log("you can't caste vote");
+}
+// else if
+let newbie=true;
+let user=true;
+let goldenuser=false;
+if(newbie){
+    console.log("0% discount");
+}
+else if(user){
+    console.log("40% discount");
+}
+else if(goldenuser){
+    console.log("80% discount");
+}
+
+
+// switch statement
+let activity="Lunch";
+switch(activity){
+    case "Get up":
+    console.log("It is 6 O'clock");
+    break
+    case "Breakfast":
+    console.log("It is 7 O'clock");
+    break
+    case "Drive to work":
+        console.log("Its 9 o'clock");
+    break
+    case "Lunch":
+        console.log("Its 1:30 o'clock");
+    break
+    case "Drive home":
+        console.log("Its 8:00 o'clock");
+    break
+    case "Dinner":
+        console.log("Its 9:30 o'clock");
+    default:
+        console.log("I am not well");
+
+}
